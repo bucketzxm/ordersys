b94386830ede5d9c1e97aed1447893b9a594d24a
SimonZheng <bucketzxm@gmail.com>
2015-05-09 21:34:02 +0800

    register admin and add verbose name for class

--- WeiyueOrderSys/ordersys/admin.py
+++ WeiyueOrderSys/ordersys/admin.py
@@ -1,3 +1,10 @@
+#!-*- coding: utf-8 -*-
 from django.contrib import admin
 
 # Register your models here.
+
+from models import Food, Category, Order
+
+admin.site.register(Food)
+admin.site.register(Category)
+admin.site.register(Order)
--- WeiyueOrderSys/ordersys/models.py
+++ WeiyueOrderSys/ordersys/models.py
@@ -2,6 +2,63 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    id = models.AutoField(primary_key=True, verbose_name="目录编号")  # 唯一编号
+    name = models.CharField(max_length=128, verbose_name="目录名称")  # 调料名字
+    image_url = models.URLField(default="", verbose_name="图片路径")  # 图片路径
+    description = models.TextField(verbose_name="调料描述")
+
+    class Meta:
+        verbose_name = "目录"
+        verbose_name_plural = "目录"
+
+    def __str__(self):
+        return "目录 " + self.id + " : " + self.name;
+
+    def __unicode__(self):
+        return "目录 " + self.id + " : " + self.name;
+
+
+class Sauce(models.Model):
+    id = models.AutoField(primary_key=True, verbose_name="调料编号")  # 唯一编号
+    name = models.CharField(max_length=128, verbose_name="调料名字")  # 调料名字
+    price = models.FloatField(default=0, verbose_name="单品价格")  # 单品价格
+    image_url = models.URLField(default="", verbose_name="图片路径")  # 图片路径
+    description = models.TextField(verbose_name="调料描述")
+
+    class Meta:
+        verbose_name = "调料"
+        verbose_name_plural = "调料"
+
+    def __str__(self):
+        return "调料 " + self.id + " : " + self.name;
+
+    def __unicode__(self):
+        return "调料 " + self.id + " : " + self.name;
+
+
+class Food(models.Model):
+    id = models.AutoField(primary_key=True, verbose_name="食物编号")  # 食物唯一编号
+    name = models.CharField(max_length=128, verbose_name="食物名称")
+
+    category = models.ForeignKey(Category, verbose_name="类别")  # 食物类别
+    image_url = models.URLField(default="", verbose_name="图片路径")
+    price = models.FloatField(default=0, verbose_name="食物价格")
+    description = models.TextField(verbose_name="食物描述")
+    special = models.BooleanField(default=False, verbose_name="是否特价")
+
+    sauces = models.ManyToManyField(Sauce, verbose_name="调料")  #
+
+    class Meta:
+        verbose_name = "食物"
+        verbose_name_plural = "食物"
+
+    def __str__(self):
+        return "食物 " + self.id + " : " + self.name;
+
+    def __unicode__(self):
+        return "食物 " + self.id + " : " + self.name;
+
 
 class Order(models.Model):
     id = models.AutoField(primary_key=True)
@@ -19,54 +76,12 @@ class Order(models.Model):
 
     foods = models.ManyToManyField(Food, verbose_name="食物")
 
+    class Meta:
+        verbose_name = "订单"
+        verbose_name_plural = "订单"
+
     def __str__(self):
         return "订单: " + self.out_trade_num;
 
     def __unicode__(self):
         return "订单: " + self.out_trade_num;
-
-
-class Food(models.Model):
-    id = models.AutoField(primary_key=True, verbose_name="食物编号")  # 食物唯一编号
-    name = models.CharField(max_length=128, verbose_name="食物名称")
-
-    category = models.ForeignKey(Category, verbose_name="类别")  # 食物类别
-    image_url = models.URLField(default="", verbose_name="图片路径")
-    price = models.FloatField(default=0)
-    description = models.TextField(verbose_name="食物描述")
-    special = models.BooleanField(default=False, verbose_name="是否特价")
-
-    sauces = models.ManyToManyField(Sauce, verbose_name="调料")  #
-
-    def __str__(self):
-        return "食物 " + self.id + " : " + self.name;
-
-    def __unicode__(self):
-        return "食物 " + self.id + " : " + self.name;
-
-
-class Sauce(models.Model):
-    id = models.AutoField(primary_key=True, verbose_name="调料编号")  # 唯一编号
-    name = models.CharField(max_length=128, verbose_name="调料名字")  # 调料名字
-    price = models.FloatField(default=0, verbose_name="单品价格")  # 单品价格
-    image_url = models.URLField(default="", verbose_name="图片路径")  # 图片路径
-    description = models.TextField(verbose_name="调料描述")
-
-    def __str__(self):
-        return "调料 " + self.id + " : " + self.name;
-
-    def __unicode__(self):
-        return "调料 " + self.id + " : " + self.name;
-
-
-class Category(models.Model):
-    id = models.AutoField(primary_key=True, verbose_name="目录编号")  # 唯一编号
-    name = models.CharField(max_length=128, verbose_name="目录名称")  # 调料名字
-    image_url = models.URLField(default="", verbose_name="图片路径")  # 图片路径
-    description = models.TextField(verbose_name="调料描述")
-
-    def __str__(self):
-        return "目录 " + self.id + " : " + self.name;
-
-    def __unicode__(self):
-        return "目录 " + self.id + " : " + self.name;
