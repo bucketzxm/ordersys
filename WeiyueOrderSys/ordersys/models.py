898d1ab2e62853128d50640dfbea533d6babbfc4
SimonZheng <bucketzxm@gmail.com>
2015-05-09 15:17:08 +0800

    django init

--- WeiyueOrderSys/ordersys/models.py
+++ WeiyueOrderSys/ordersys/models.py
@@ -1,3 +1,10 @@
+#!-*- coding:utf-8 -*-
 from django.db import models
 
 # Create your models here.
+
+class Order(models.Model):
+    id = models.AutoField(primary_key=True)
+    out_trade_num = models.CharField(max_length=128, verbose_name="订单号")
+    order_state = models.CharField(max_length=50,verbose_name="订单状态")
+    
